e97f64494d37ba834a6d826edcdb7871bea98079
kbeerta <koenbeerta@gmail.com>
2023-08-11 15:17:57 +0200

    bump nvim-config version

--- modules/programs/nvim/home.nix
+++ modules/programs/nvim/home.nix
@@ -5,7 +5,7 @@
     source = pkgs.fetchFromGitHub {
       owner = "kbeerta";
       repo = "nvim-config";
-      rev = "0adad2e";
+      rev = "358dfac";
       sha256 = "Vc3ZXf/R2OtGa7trfqdcEmfTx3CPSXhtWKltah21wSw=";
     };
   };
